d38327eb68f75d96807fe175cca3e54ce11b761a
unknown <sjyetki@BANL07524372E.corp.intuit.net>
2015-04-18 15:36:45 +0530

    ftt Change

--- src/main/java/com/springrolepeople/App.java
+++ src/main/java/com/springrolepeople/App.java
@@ -1,13 +1,20 @@
 package com.springrolepeople;
 
 /**
- * Hello world!
+ * This is app class 
  *
  */
 public class App 
 {
+	/** Main Method**/
     public static void main( String[] args )
     {
         System.out.println( "Hello World! with git hub" );
     }
+	
+	/** Sample  Method**/
+	public void check()
+	{
+		System.out.println( "Hello=================" );
+	}
 }
